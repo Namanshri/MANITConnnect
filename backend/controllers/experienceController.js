2140f85bd5ba096e2fbd466b4abe1c68d40d2909
Namanshri <namanshrivastavaofficial@gmail.com>
2026-07-12 22:13:39 +0530

    backend issue update fix

--- backend/controllers/experienceController.js
+++ backend/controllers/experienceController.js
@@ -24,7 +24,7 @@ const createExperience = async (req, res) => {
 
         const result = await pool.query(
 
-            `INSERT INTO insights
+            `INSERT INTO experiences
             (
                 mentor_id,
                 preparation_strategy,
